4c64c170afc966e4d55066f26bd0cf5e156ca41a
Ashutosh Farswan <54589110+ashufarswan@users.noreply.github.com>
2022-10-10 23:22:05 +0530

    fix  cam lag issue

--- Emojify/src/emoji.py
+++ Emojify/src/emoji.py
@@ -1,3 +1,4 @@
+from glob import glob
 import tkinter as tk
 from tkinter import *
 import cv2
@@ -22,8 +23,9 @@ emotion_dict = {
     7 : ''
 }
 
-cur_path = os.path.dirname(os.path.abspath(__file__))
 
+cur_path = os.path.dirname(os.path.abspath(__file__))
+cur_path = cur_path[:-4]
 emoji_dict ={
     0 : cur_path + "/Data/Emojies/angry.png",
     1 : cur_path + "/Data/Emojies/disgust.png",
@@ -42,7 +44,7 @@ prediction = 7
 
 
 width, height = 500, 500
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture(0)
 cap.set(cv2.CAP_PROP_FRAME_WIDTH, width)
 cap.set(cv2.CAP_PROP_FRAME_HEIGHT, height)
 
@@ -57,28 +59,41 @@ text.pack()
 emoji.pack(side =RIGHT)
 
 
-def show_frame():
-    _, frame = cap.read()
-    bounding_box = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    num_faces = bounding_box.detectMultiScale(gray_frame,scaleFactor=1.3, minNeighbors=5)
-    for (x, y, w, h) in num_faces:
-        cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
-        cropped_img = gray_frame[y:y + h, x:x + w]
-        cropped_img = cv2.resize(cropped_img, (48, 48))
-        cropped_img = np.array(cropped_img)
-        cropped_img = cropped_img.reshape(1,48,48,1)
-        cropped_img = cropped_img/255.0 
-        global prediction
-        prediction = np.argmax(model.predict(cropped_img))
+event  = threading.Event()
 
-    frame = cv2.flip(frame, 1)
-    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
-    img = Image.fromarray(cv2image)
-    imgtk = ImageTk.PhotoImage(image=img)
-    cam.imgtk = imgtk
-    cam.configure(image=imgtk)
-    cam.after(10, show_frame)
+
+def on_closing():
+    event.set()
+    root.destroy()
+
+
+root.protocol("WM_DELETE_WINDOW", on_closing)
+
+def show_frame():
+    while True:
+        _, frame = cap.read()
+        bounding_box = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        num_faces = bounding_box.detectMultiScale(gray_frame,scaleFactor=1.3, minNeighbors=5)
+        for (x, y, w, h) in num_faces:
+            cv2.rectangle(frame, (x, y-50), (x+w, y+h+10), (255, 0, 0), 2)
+            cropped_img = gray_frame[y:y + h, x:x + w]
+            cropped_img = cv2.resize(cropped_img, (48, 48))
+            cropped_img = np.array(cropped_img)
+            cropped_img = cropped_img.reshape(1,48,48,1)
+            cropped_img = cropped_img/255.0 
+            global prediction
+            prediction = np.argmax(model.predict(cropped_img))
+
+        frame = cv2.flip(frame, 1)
+        cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+        img = Image.fromarray(cv2image)
+        imgtk = ImageTk.PhotoImage(image=img)
+        cam.imgtk = imgtk
+        cam.configure(image=imgtk)
+        if event.is_set() :
+            break
+            #cam.after(10, show_frame)
 
 def show_avatar():
     frame2=cv2.imread(emoji_dict[prediction])
@@ -88,9 +103,11 @@ def show_avatar():
     emoji.imgtk=imgtk
     text.configure(text=emotion_dict[prediction],font=('arial',45,'bold'))
     emoji.configure(image=imgtk)
-    emoji.after(10, show_avatar)
+    if not event.is_set() :
+        emoji.after(10, show_avatar)
 
-threading.Thread(target = show_frame).start()
+threading.Thread(target=show_frame).start()
 threading.Thread(target = show_avatar).start()
+
 
 root.mainloop()
